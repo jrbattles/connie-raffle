055c0079f4c3ccd80059ec18e7168453457d9cb4
Jason R. Battles <jason.r.battles@gmail.com>
2023-06-08 16:30:08 -0500

    init tts selection. err to PulseAudio

--- docker-random-person-tts/random_person_tts.py
+++ docker-random-person-tts/random_person_tts.py
@@ -1,61 +1,39 @@
 import random
 import time
-
+from gtts import gTTS
+import os
 
 # Open the text file containing the list of people
 with open('people.txt', 'r') as file:
     people_list = file.read().splitlines()
 
-print()
-print("ACTIVATING CONNIE ", end='', flush=True)
-for _ in range(3):
-    print("*", end='', flush=True)
-    time.sleep(1)  # Add a delay of 1 second for suspense
-print()
-print()
+mytext01 = "I am scrambling the list of attendees now."
 
-
-# Print "thinking..." to create suspense
-print("Hi, I am Connie the Containerized Tech Crusader.")
-
-print("I am scrambling the list of attendees now.", end='', flush=True)
-for _ in range(3):
-    print(".", end='', flush=True)
-    time.sleep(1)  # Add a delay of 1 second for suspense
-
-# time.sleep(2)  # Add a delay of 3 seconds for suspense
-print("ok")
-
-# time.sleep(3)  # Add a delay of 3 seconds for suspense
-print("And I am randomizing again based on quantum conditions.", end='', flush=True)
-for _ in range(3):
-    print(".", end='', flush=True)
-    time.sleep(1)  # Add a delay of 1 second for suspense
-
-print("ok")
-# time.sleep(3)  # Add a delay of 3 seconds for suspense
-# print("The winner of the Tech Crusaders Raffle is...")
-# time.sleep(3)  # Add a delay of 3 seconds for suspense
-
-# Print "thinking..." to create suspense
-print("Thinking.", end='', flush=True)
-time.sleep(1)  # Add a delay of 1 second for suspense
-
-for _ in range(3):
-    print(".", end='', flush=True)
-    time.sleep(1)  # Add a delay of 1 second for suspense
-
-print("ok")
-
-print("The randomly selected person is.", end='', flush=True)
-time.sleep(1)  # Add a delay of 1 second for suspense
-
-for _ in range(3):
-    print(".", end='', flush=True)
-    time.sleep(1)  # Add a delay of 1 second for suspense
+time.sleep(3)  # Add a delay of 3 seconds for suspense
+mytext02 = "And I am randomizing again based on quantum conditions."
+mytext03 = "Thinking."
+mytext04 = "ok"
+mytext05 = "The randomly selected person is."
 # Randomly select a person from the list
-
-random_person = random.choice(people_list)
+#random_person = random.choice(people_list)
 
 # Print the randomly selected person
-print(f"The randomly selected person is: {random_person}")
+#mytext06 = ("The randomly selected person is: {random_person}")
+
+time.sleep(2)  # Add a delay of 1 second for suspense
+
+# Language in which you want to convert
+language = 'en'
+
+# Passing the text and language to the engine,
+# here we have marked slow=False. Which tells
+# the module that the converted audio should
+# have a high speed
+myobj = gTTS(text=mytext01, lang=language, slow=False)
+
+# Saving the converted audio in a mp3 file named
+# welcome
+myobj.save("text01.mp3")
+
+# Playing the converted file
+os.system("mpg321 text01.mp3")
