79db8371972c96c7c5c3b1fb874ea7932fb4cd14
Jason R. Battles <jason.r.battles@gmail.com>
2023-06-28 15:46:26 -0500

    dress rehearsal updates

--- docker-random-person-tts/03-Connie.py
+++ docker-random-person-tts/03-Connie.py
@@ -8,7 +8,7 @@ import os
 import time
 
 # The text that you want to convert to audio
-mytext = "My formal name is Connie the Containerized Tech Crusader. I am here to add a little technical flavor and inject some fun into our team events.  I am not yet as smart and capable as my big sister Clara. However, I do have aspirations to join our team activities, perhaps starting with our Content Creation and Form Your Own Opinion workstreams..  At the end of today's meeting, I also have been tasked with randomly selecting a raffle winner, so that will be fun"
+mytext = "My formal name is Connie the Containerized Tech Crusader. I was created to add a little technical flavor and inject some fun into our team events.  I am not yet as smart and capable as my big sister Clara, but SHE does not have a cool helmet and sword..  I have aspirations to join our team activities, perhaps starting with our Content Creation and Form Your Own Opinion workstreams..  At the end of today's meeting, I also have been tasked with randomly selecting a raffle winner, so that will be fun."
 mytextb = "Hey James, I even have a joke for you.  How many digital humans does it take to change a light bulb?"
 
 # Language in which you want to convert
